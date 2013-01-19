75ef7f8b3cfb60b75b3810f618ef3b857a374c87
Aleksey Tsuber <LexeY4eg@gmail.com>
2013-01-19 20:32:59 +0300

    remove processed boot updated

--- app/jobs/UpdateDataModel.java
+++ app/jobs/UpdateDataModel.java
@@ -15,45 +15,45 @@ public class UpdateDataModel extends Job {
 
 	public void doJob() {
 		
-		List<User> users = User.findAll();
-		for (User user : users) {
-			if (user.english == null) {
-				user.english = false;
-				user.save();
-			}
-		}
+//		List<User> users = User.findAll();
+//		for (User user : users) {
+//			if (user.english == null) {
+//				user.english = false;
+//				user.save();
+//			}
+//		}
+//		
+//		List<Command> commandsWithEmptyGlobal = Command.find("global is NULL").fetch();
+//		for (Command c : commandsWithEmptyGlobal) {
+//			c.global = false;
+//			c.save();
+//		}
+//		
+//		List<Post> postsWithEmptyLang = Post.find("lang is NULL").fetch();
+//		for (Post post : postsWithEmptyLang) {
+//			post.lang = 0;
+//			post.save();
+//		}
 		
-		List<Command> commandsWithEmptyGlobal = Command.find("global is NULL").fetch();
-		for (Command c : commandsWithEmptyGlobal) {
-			c.global = false;
-			c.save();
-		}
-		
-		List<Post> postsWithEmptyLang = Post.find("lang is NULL").fetch();
-		for (Post post : postsWithEmptyLang) {
-			post.lang = 0;
-			post.save();
-		}
-		
-		List<Command> commands = Command.findAll();
-		for (Command command : commands) {
-			for (Topic topic : command.topics) {
-				if (topic.mainTopic) {
-					topic.publicTopic = false;
-					topic.groupId = command.id;
-					topic.save();
-				}
-			}
-			// creating public main topic
-			Topic mainPublicTopic = new Topic();
-			mainPublicTopic.mainTopic = true;
-			mainPublicTopic.publicTopic = true;
-			mainPublicTopic.groupId = command.id;
-			mainPublicTopic.save();
-			
-			command.topics.add(mainPublicTopic);
-			command.save();
-		}
+//		List<Command> commands = Command.findAll();
+//		for (Command command : commands) {
+//			for (Topic topic : command.topics) {
+//				if (topic.mainTopic) {
+//					topic.publicTopic = false;
+//					topic.groupId = command.id;
+//					topic.save();
+//				}
+//			}
+//			// creating public main topic
+//			Topic mainPublicTopic = new Topic();
+//			mainPublicTopic.mainTopic = true;
+//			mainPublicTopic.publicTopic = true;
+//			mainPublicTopic.groupId = command.id;
+//			mainPublicTopic.save();
+//			
+//			command.topics.add(mainPublicTopic);
+//			command.save();
+//		}
 		
 	}
 
