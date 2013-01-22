49ab7491a138b37c23e752b74dd752fcdca45e8e
Aleksey Tsuber <LexeY4eg@gmail.com>
2013-01-23 00:03:37 +0300

    33	Выбор языка для пользователя слетает при закрытии браузера.

--- app/controllers/LocaleController.java
+++ app/controllers/LocaleController.java
@@ -6,6 +6,7 @@ import play.Play;
 import play.i18n.Lang;
 import play.mvc.Before;
 import play.mvc.Controller;
+import play.mvc.Http.Response;
 import utils.SessionHelper;
 
 public class LocaleController extends BasicController {
@@ -13,7 +14,9 @@ public class LocaleController extends BasicController {
 	public static void change(String locale) {
 //		SessionHelper.setCurrentLocale(session, locale);
 //		System.out.println("locale change to: " + locale);
-		Lang.change(locale);
+		//Lang.change(locale);
+		Lang.set(locale);
+		Response.current().setCookie(Play.configuration.getProperty("application.lang.cookie", "PLAY_LANG"), locale, "365d");
 		System.out.println("locale change to: " + locale + " langs=" + Play.langs);
 		renderText("ok");
 	}
