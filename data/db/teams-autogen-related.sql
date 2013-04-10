3ff48b76923fc1ee3ff69767e1fa4ee81e0ab9b9
Siarzh Miadzvedzeu <siarzh@gmail.com>
2013-04-10 17:03:04 +0300

    Fixed CrowdDeveloping & CrowdConsulting form submit

--- data/db/teams-autogen-related.sql
+++ data/db/teams-autogen-related.sql
@@ -4,9 +4,6 @@ update User set takePartInAutoTeams = 1 where role <> 0;
 /* removes all empty teams */
 delete c.* from Command c where (select count(*) from User u where u.command_id = c.id) = 0;
 
-/* modifies (actually corrupts) all user emails for safe testing */
-update User set email = CONCAT(email, '.test') where id not in (1, 91, 103, 106, 177);
-
 /* update Command related foreign keys restrictions */
 ALTER TABLE `TeamMemberActivity` DROP FOREIGN KEY `FK65C48BA6F0221DE4`;
 ALTER TABLE `TeamMemberActivity` ADD CONSTRAINT `FK65C48BA6F0221DE4` FOREIGN KEY (`team_id`) REFERENCES `Command` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
@@ -18,5 +15,8 @@ ALTER TABLE `Command_Topic` ADD CONSTRAINT `FK4C856AFB74A93236` FOREIGN KEY (`Co
 ALTER TABLE `Command_Topic` DROP FOREIGN KEY `FK4C856AFBC26F92A1`;
 ALTER TABLE `Command_Topic` ADD CONSTRAINT `FK4C856AFBC26F92A1` FOREIGN KEY (`topics_id`) REFERENCES `Topic` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/* TEST: detete all user notifications */
-DELETE FROM `User_NotificationType`
+/* TEST DB ONLY: detete all user notifications */
+DELETE FROM `User_NotificationType`;
+
+/* TEST DB ONLY: modifies (actually corrupts) all user emails for safe testing */
+update User set email = CONCAT(email, '.test') where id not in (1, 91, 103, 106, 177);
